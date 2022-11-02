d211052c057170b7f252c50e80388157d39de17c
Valery Krukov <valery.v.krukov@gmail.com>
2022-11-02 21:00:28 +0200

    Main setup

--- config/bundles.php
+++ config/bundles.php
@@ -2,4 +2,6 @@
 
 return [
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
+    Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
+    Symfony\UX\Turbo\TurboBundle::class => ['all' => true],
 ];
